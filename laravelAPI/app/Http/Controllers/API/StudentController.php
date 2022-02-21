d95734d7fdc9fab2184a81f29abd07e12d4adda7
emmageo3 <kanfanyemma22@gmail.com>
2022-02-21 10:44:14 +0000

    done

---
+++ laravelAPI/app/Http/Controllers/API/StudentController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+        return response()->json([
+            'status'=> 200,
+            'students'=>$students,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|max:191',
+            'course'=>'required|max:191',
+            'email'=>'required|email|max:191',
+            'phone'=>'required|max:10|min:10',
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json([
+                'status'=> 422,
+                'validate_err'=> $validator->messages(),
+            ]);
+        }
+        else
+        {
+            $student = new Student;
+            $student->name = $request->input('name');
+            $student->course = $request->input('course');
+            $student->email = $request->input('email');
+            $student->phone = $request->input('phone');
+            $student->save();
+
+            return response()->json([
+                'status'=> 200,
+                'message'=>'Student Added Successfully',
+            ]);
+        }
+
+    }
+
+    public function edit($id)
+    {
+        $student = Student::find($id);
+        if($student)
+        {
+            return response()->json([
+                'status'=> 200,
+                'student' => $student,
+            ]);
+        }
+        else
+        {
+            return response()->json([
+                'status'=> 404,
+                'message' => 'No Student ID Found',
+            ]);
+        }
+
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|max:191',
+            'course'=>'required|max:191',
+            'email'=>'required|email|max:191',
+            'phone'=>'required|max:10|min:10',
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json([
+                'status'=> 422,
+                'validationErrors'=> $validator->messages(),
+            ]);
+        }
+        else
+        {
+            $student = Student::find($id);
+            if($student)
+            {
+
+                $student->name = $request->input('name');
+                $student->course = $request->input('course');
+                $student->email = $request->input('email');
+                $student->phone = $request->input('phone');
+                $student->update();
+
+                return response()->json([
+                    'status'=> 200,
+                    'message'=>'Student Updated Successfully',
+                ]);
+            }
+            else
+            {
+                return response()->json([
+                    'status'=> 404,
+                    'message' => 'No Student ID Found',
+                ]);
+            }
+        }
+    }
+
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+        if($student)
+        {
+            $student->delete();
+            return response()->json([
+                'status'=> 200,
+                'message'=>'Student Deleted Successfully',
+            ]);
+        }
+        else
+        {
+            return response()->json([
+                'status'=> 404,
+                'message' => 'No Student ID Found',
+            ]);
+        }
+    }
+}
--- laravelAPI/routes/api.php
+++ laravelAPI/routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\StudentController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('students', [StudentController::class, 'index']);
+Route::post('/add-student', [StudentController::class, 'store']);
+Route::get('/edit-student/{id}', [StudentController::class, 'edit']);
+Route::put('update-student/{id}', [StudentController::class, 'update']);
+Route::delete('delete-student/{id}', [StudentController::class, 'destroy']);
